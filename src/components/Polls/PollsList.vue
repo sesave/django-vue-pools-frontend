ecbc11705d774934e83259feee2585b16f20324d
Sebastian Sánchez <s.sanchez.vega@accenture.com>
2023-03-07 15:34:45 -0300

    Updated: Pollslist

--- src/components/Polls/PollsList.vue
+++ src/components/Polls/PollsList.vue
@@ -1,36 +1,77 @@
 <template>
-  <div class="hello">
-    <h1>Polls</h1>
+  <div class="pollsWrapper">
+    <h1>Enquetes</h1>
+    <div class="pollGrid">
+      <div v-for="poll in polls" v-bind:key="poll.id" class="poll">
+        <v-card
+          variant="tonal"
+          class="transition-fast-in-fast-out v-card--reveal"
+        >
+          <v-card-title v-text="poll.question_text"></v-card-title>
+          <v-card-text v-text="this.formatDate(poll.pub_date)"></v-card-text>
+          <v-card-actions>
+            <v-btn variant="outlined" @click="redirectVote(poll.id)">
+              Vote
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
+import router from '../../router/'
 export default {
-  name: "PollsList", 
-
-  mounted () {
-    axios
-      .get('http://localhost:8081/polls/')
-      .then(response => (this.info = response))
+  name: "PollsList",
+  data() {
+    return {
+      polls: null,
+    };
+  },
+  methods: {
+    async getPolls() {
+      await axios
+        .get("http://localhost:8081/polls/")
+        .then((response) => (this.polls = response.data.questions));
+    },
+    formatDate(dateString) {
+      const date = new Date(dateString);
+      return new Intl.DateTimeFormat("pt-BR", {
+        dateStyle: "full",
+        timeStyle: "long",
+        timeZone: "America/Sao_Paulo",
+      }).format(date);
+    },
+    redirectVote(id) {
+      router.push({ name: "Poll Details", params: { id } });
+    },
+  },
+  created() {
+    this.getPolls();
+  },
+  updated() {
+    console.log(this.polls);
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.pollsWrapper {
+  max-width: 1440px;
+  margin: 0 auto;
+  padding-top: 2rem;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.poll {
+  margin-top: 1rem;
+  width: fit-content;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.pollGrid {
+  display: grid;
+  grid-auto-flow: column;
+  gap: 1rem;
+  grid-auto-columns: max-content;
 }
 </style>
