655c6ad488aa74eed81c2929670f750905438abe
Sebastian Sánchez <s.sanchez.vega@accenture.com>
2023-03-07 15:28:06 -0300

    Done: App

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,10 @@
-<template>
-  <PollsList />
-</template>
-
 <script>
-import PollsList from './components/Polls/PollsList'
-export default { 
-  name: 'App',
-  components: {
-    PollsList
-  }
-}
+
+export default {
+  name: "App"
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<template>
+  <router-view />
+</template>
